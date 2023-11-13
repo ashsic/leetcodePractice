9ca207042c9a446ab9c213e237bebc3eac0fd85b
ashsic <99445200+ashsic@users.noreply.github.com>
2023-11-13 10:09:36 -0500

    added iterative, inorder BST traversal sol'ns

--- 94_Binary_Tree_Inorder_Traversal.py
+++ 94_Binary_Tree_Inorder_Traversal.py
@@ -22,3 +22,28 @@ class Solution:
         
         data = traverse(root, [])
         return data
+    
+# Based on learning iterative inorder BST traversal for #230 via Neetcode, created this sol'n:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            inorder.append(curr.val)
+
+            curr = curr.right
+        
+        return inorder
