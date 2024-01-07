5ef95c42118a95c61de3e51a690b1f660eec2f72
ashsic <99445200+ashsic@users.noreply.github.com>
2024-01-07 13:26:38 -0500

    more fast/slow pointer solutions

--- 287_Find_the_Duplicate_Number.py
+++ 287_Find_the_Duplicate_Number.py
@@ -9,3 +9,22 @@ class Solution:
                 return num
             else:
                 data.add(num)
+
+# fast/slow pointer solution
+                
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            if slow2 == slow:
+                return slow2
+            slow2 = nums[slow2]
+            slow = nums[slow]
