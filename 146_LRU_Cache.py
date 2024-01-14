6448f719acebdea3c36a76615805000250756f77
ashsic <99445200+ashsic@users.noreply.github.com>
2024-01-14 12:20:22 -0500

    practicing this one and modified it slightly

--- 146_LRU_Cache.py
+++ 146_LRU_Cache.py
@@ -52,3 +52,59 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+        
+
+
+# Solution V2
+
+class ListNode:
+    def __init__(self, key=-1, val=-1, next=None, prev=None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left = ListNode()
+        self.right = ListNode()
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.insert(self.cache[key].key, self.cache[key].val)
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) == self.capacity and key not in self.cache:
+            self.cache.pop(self.right.prev.key)
+            self.right.prev = self.right.prev.prev
+            self.right.prev.next = self.right
+            
+        self.insert(key, value)
+
+    def insert(self, key, value):
+        if key in self.cache:
+            self.cache[key].val = value
+            self.cache[key].next.prev = self.cache[key].prev
+            self.cache[key].prev.next = self.cache[key].next
+            self.cache[key].next = self.left.next
+            self.cache[key].prev = self.left
+        else:
+            self.cache[key] = ListNode(key, value, self.left.next, self.left)
+        self.left.next.prev = self.cache[key]
+        self.left.next = self.cache[key]
+        
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
